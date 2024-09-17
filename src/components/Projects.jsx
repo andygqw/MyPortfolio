0457c54f5950fc8d467280428a5caa591cdd2275
andygqw <andygqwang@gmail.com>
2024-09-16 18:00:56 -0700

    added projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -73,18 +73,108 @@ const TimeAndDetails = styled(Box)({
   display: 'flex',
   justifyContent: 'space-between',
   fontSize: '1rem',
+  marginBottom: '20px',
 });
+
+const GitHubButton = ({ url }) => {
+  return (
+    <Button
+      variant="contained"
+      onClick={() => {
+        window.open(url, '_blank');
+      }}
+      sx={{
+        // marginBottom: '40px',
+        marginRight: '8px',
+        backgroundColor: 'grey',
+        padding: '2px 8px',
+        borderRadius: '8px',
+        color: 'black',
+        fontWeight: 'bold',
+        '&:hover': {
+          backgroundColor: 'white',
+        },
+      }}
+    >
+      <GitHubIcon
+        sx={{ fontSize: 20, color: '#000', marginRight: '4px' }}
+      />
+      Github</Button>
+  );
+};
+
+const DemoButton = ({ url }) => {
+  return (
+    <Button
+      variant="contained"
+      onClick={() => {
+        window.open(url, '_blank');
+      }}
+      sx={{
+        // marginBottom: '40px',
+        backgroundColor: 'grey',
+        padding: '2px 8px',
+        borderRadius: '8px',
+        color: 'black',
+        fontWeight: 'bold',
+        '&:hover': {
+          backgroundColor: 'white',
+        },
+      }}
+    >
+      <FileOpenIcon
+        sx={{ fontSize: 20, color: '#000', marginRight: '4px' }}
+      />
+      Demo
+    </Button>
+  );
+};
+
+const ProjectDetails = ({ purpose, highlight, image, alt }) => {
+  return (
+    <Box sx={{
+      display: 'flex',
+      justifyContent: 'space-between',
+      //alignItems: 'center',
+      flexDirection: 'row',
+    }}>
+      <Box sx={{width:'50%'}}>
+        <Typography sx={{
+          whiteSpace:'pre-line',
+          textAlign:'left',
+          fontSize: '1.2rem',
+          lineHeight: '1.75',
+          marginBottom: '20px',
+        }}>
+          {purpose}
+        </Typography>
+        <Typography sx={{
+          whiteSpace:'pre-line',
+          textAlign:'left',
+          fontSize: '1.2rem',
+          fontWeight: 'bold',
+          lineHeight: '1.5',
+        }}>
+          {highlight}
+        </Typography>
+      </Box>
+      <Box sx={{display:'flex',alignItems:'center',width:'45%'}}>
+        <img src={image} alt={alt} style={{maxWidth:'100%', height:'fit-content'}}/>
+      </Box>
+    </Box>
+  );
+};
 
 const Projects = () => {
   return (
     <section id='projects'>
       <CareerPathContainer>
         <CareerHeader>
-          Projects
+          Selected Projects
         </CareerHeader>
 
-        <Typography variant="body1" sx={{ marginBottom: '20px', textAlign: 'center' }}>
-          Follow these key milestones to reach the pinnacle of your career.
+        <Typography variant="body1" sx={{ marginBottom: '20px', textAlign: 'center', maxWidth: '550px' }}>
+          Click on Demo buttons to view deployed projects, use username "Demo" and password "Demo" to enjoy SSO demo login across projects.
         </Typography>
 
         <Button
@@ -117,54 +207,23 @@ const Projects = () => {
           </MilestoneHeader>
           <Line />
           <TimeAndDetails>
-            <Typography>Node.JS / React / AWS S3 / CloudFlare Worker / SQLite</Typography>
+            <Typography sx={{color:'grey'}}>Node.JS / React / AWS SDK / CloudFlare Workers / SQLite</Typography>
             <div>
-              <Button
-                variant="contained"
-                onClick={() => {
-                  window.open('https://github.com/andygqw?tab=repositories', '_blank');
-                }}
-                sx={{
-                  marginBottom: '40px',
-                  marginRight: '8px',
-                  backgroundColor: 'grey',
-                  padding: '2px 8px',
-                  borderRadius: '8px',
-                  color: 'black',
-                  fontWeight: 'bold',
-                  '&:hover': {
-                    backgroundColor: 'white',
-                  },
-                }}
-              >
-                <GitHubIcon
-                  sx={{ fontSize: 20, color: '#000', marginRight: '4px' }}
-                />
-                Github</Button>
-              <Button
-                variant="contained"
-                onClick={() => {
-                  window.open('https://github.com/andygqw?tab=repositories', '_blank');
-                }}
-                sx={{
-                  marginBottom: '40px',
-                  backgroundColor: 'grey',
-                  padding: '2px 8px',
-                  borderRadius: '8px',
-                  color: 'black',
-                  fontWeight: 'bold',
-                  '&:hover': {
-                    backgroundColor: 'white',
-                  },
-                }}
-              >
-                <FileOpenIcon
-                  sx={{ fontSize: 20, color: '#000', marginRight: '4px' }}
-                />
-                Demo
-              </Button>
+              <GitHubButton url='https://github.com/andygqw/GWTool_React' />
+              <DemoButton url='https://tool.tiny-pink.com/' />
             </div>
           </TimeAndDetails>
+          <ProjectDetails
+            purpose="This project is a cloud-based efficiency boost application that transfers and manipulates text, shares resources, and manages cloud files storage.
+            It also incorporates user authentication, permission-based access to resources, and Facebook type posts management."
+            highlight="- Integration with Cloudflare R2 via AWS SDK.
+            - Secure file operations using presigned URLs.
+            - Single Sign On across multiple applications.
+            - Cookie-based JWT token for resource access.
+            - Post and reply system with file attachments."
+            image="src/static/gwtool.png"
+            alt="gwtool"
+          />
         </CareerMilestone>
 
         <CareerMilestone>
@@ -174,9 +233,23 @@ const Projects = () => {
           </MilestoneHeader>
           <Line />
           <TimeAndDetails>
-            <Typography>Java / OpenAI Whisper / CloudFlare Worker AI</Typography>
-            {/* <Typography></Typography> */}
+            <Typography sx={{color:'grey'}}>Java / OpenAI Whisper / CloudFlare Worker AI</Typography>
+            <div>
+              <GitHubButton url='https://github.com/andygqw/SubtitleGenerator' />
+              {/* <DemoButton url='https://github.com/andygqw?tab=repositories' /> */}
+            </div>
           </TimeAndDetails>
+          <ProjectDetails
+            purpose="This project is a Java-based tool designed to generate subtitles for large number of long video or audio files by transcribing audios with OpenAI Whisper model through Cloudflare Workers AI.
+            The tool handles multiple large files at once, making it highly efficient for bulk video processing."
+            highlight="- Transcribe multiple large videos simultaneously.
+            - Multithreading and concurrency for 15x speedups.
+            - Semaphore-based resources in multi-threading.
+            - Integrates with speech-to-text AI model.
+            - Performance-tuned and fault tolerance."
+            image="src/static/whisper.jpeg"
+            alt="whisper"
+          />
         </CareerMilestone>
 
         <CareerMilestone>
@@ -186,9 +259,17 @@ const Projects = () => {
           </MilestoneHeader>
           <Line />
           <TimeAndDetails>
-            <Typography>Node.JS / CloudFlare Pages / MySQL</Typography>
-            {/* <Typography></Typography> */}
+            <Typography sx={{color:'grey'}}>Node.JS / CloudFlare Pages / MySQL</Typography>
+            <div>
+              <GitHubButton url='https://github.com/andygqw/JATReact-Worker' />
+              <DemoButton url='https://jat.tiny-pink.com/' />
+            </div>
           </TimeAndDetails>
+          <ProjectDetails
+            purpose=""
+            highlight=""
+            alt="jat"
+          />
         </CareerMilestone>
       </CareerPathContainer>
     </section>
